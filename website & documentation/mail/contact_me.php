9faa08b8a5b74065f67c65fa80e64af3acbcea2a
Dhruvil Gandhi <dhruv.scorpio@gmail.com>
2015-10-14 02:40:51 -0400

    doc update

---
+++ website & documentation/mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+
+$to = 'dhruv@dhruvgandhi.me';
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: dhruv@dhruvgandhi.me\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>
